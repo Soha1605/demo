1823093a28926d774f9e3e6474a86b6e38e03cfd
Ngoc Thuan <lengocthuan200502@gmail.com>
2023-05-23 21:17:21 +0700

    Nop bai tuan 3

--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -3,12 +3,15 @@ package com.example.demo;
 
 import com.example.demo.entity.Book;
 import com.example.demo.services.BookService;
+import com.example.demo.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -19,12 +22,60 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping
     public String showAllBooks(Model model){
         List<Book> books = bookService.getAllBooks();
         model.addAttribute("books", books);
         return "book/list";
     }
+
+    @GetMapping("/add")
+    public String addBookForm(Model model){
+        model.addAttribute("book", new Book());
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "book/add";
+    }
+
+    @PostMapping("/add")
+    public String addBook(@Validated @ModelAttribute("book") Book book, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("book", new Book());
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/add";
+        }
+        bookService.addBook(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBookForm (@PathVariable("id") Long id, Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
+        Book editBook = bookService.getBookById(id);
+
+        if (editBook != null) {
+            model.addAttribute("book", editBook);
+            return "book/edit";
+        }else {
+            return "not-found";
+        }
+    }
+    @PostMapping("/edit")
+    public String editBook (@ModelAttribute("book") Book updatedBook) {
+
+        bookService.updateBook(updatedBook);
+        return "redirect:/books";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        Book book = bookService.getBookById(id);
+        bookService.deleteBook(id);
+        return "redirect:/books";
+    }
+
+
 
 }
 
